78cf338c4bf6999218d6679dd0a4dd9f023ce800
Rohan Relan <hanrelan@yahoo.com>
2020-07-27 22:43:05 -0700

    Make simple agent block wins as well

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A simple Connect4 RL agent written in Julia and Flux. Uses a small NN to approximate the value of a Connect4 board.
 Performance could be better.
 
-LearningAgent has a 97% winrate against SimpleAgent after 300k episodes
+LearningAgent has a 84% winrate against SimpleAgent after 300k episodes
--- simple_agent.jl
+++ simple_agent.jl
@@ -7,14 +7,45 @@ end
 
 start_simple_agent() = SimpleAgent()
 
-function get_value(game_state::Connect4.GameState)
-    (game_state.status == Connect4.player1_win || game_state.status == Connect4.player2_win) ? 101 : rand(1:100)
+function find_winning_action(game_state, actions)
+    for i in 1:length(actions)
+        action = actions[i]
+        new_state = Connect4.act(game_state, action)[1]
+        if new_state.status == Connect4.player1_win || new_state.status == Connect4.player2_win
+            return action
+        end
+    end
+    return nothing
 end
 
 function get_action(state::SimpleAgent, game_state, actions)
-    value_of_action = (action) -> get_value(Connect4.act(game_state, action)[1])
-    (_, index) = findmax(map(value_of_action, actions))
-    actions[index]
+    # First see if we have an easy win
+    winning_action = find_winning_action(game_state, actions)
+    if winning_action !== nothing
+        return winning_action
+    end
+    
+    # Check if the opponent has an easy win - if so, don't take that action
+    new_actions = []
+    for i in 1:length(actions)
+        my_action = actions[i]
+        new_state = Connect4.act(game_state, my_action)[1]
+        their_actions = Connect4.get_actions(new_state) 
+        their_winning_action = find_winning_action(new_state, their_actions)
+        if their_winning_action === nothing
+            # They don't have a winning action, this is an acceptable choice
+            push!(new_actions, my_action)
+        end
+    end
+
+    if length(new_actions) > 0
+        index = rand(1:length(new_actions))
+        new_actions[index]
+    else
+        # Doesn't seem to matter what we do - pick a random action
+        index = rand(1:length(actions))
+        actions[index]
+    end
 end
 
 function push_state!(::SimpleAgent, game_state)
