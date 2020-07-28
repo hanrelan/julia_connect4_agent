3f431078554fc4318c770ea8a48e3229a8742678
Rohan Relan <hanrelan@yahoo.com>
2020-07-27 22:07:07 -0700

    Add discounting and exploration, reduce LR

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A simple Connect4 RL agent written in Julia and Flux. Uses a small NN to approximate the value of a Connect4 board.
 Performance could be better.
+
+LearningAgent has a 97% winrate against SimpleAgent
--- learning_agent.jl
+++ learning_agent.jl
@@ -17,12 +17,24 @@ struct LearningAgent <: Agent
     game_states
     episode_results
     model_train::ModelTrainingInfo
+    exploration_rate
+end
+
+function exponential_fill(amount, shape, discount)
+    return fill(amount, shape)
+    normal_filled = zeros(Float32, shape)
+    decayed_amount = amount
+    for i in shape[2]:-1:1
+        normal_filled[i] = decayed_amount
+        decayed_amount *= discount
+    end
+    normal_filled
 end
 
 function train!(state::LearningAgent)
     batch_rewards = []
     batch_states = []
-    batch_rewards = map(episode -> fill(episode.reward, (1, length(episode.game_states))), state.episode_results)
+    batch_rewards = map(episode -> exponential_fill(episode.reward, (1, length(episode.game_states)), 0.90), state.episode_results)
     batch_rewards =  reduce(hcat, batch_rewards)
     batch_states = map(episode -> create_batch(episode.game_states), state.episode_results)
     batch_states =  reduce(hcat, batch_states)
@@ -38,8 +50,8 @@ end
 
 function start_learning_agent() 
     model = Chain(Dense(6 * 7 * 3, 50, sigmoid), Dense(50, 10, sigmoid), Dense(10, 1, tanh))
-    model_train = ModelTrainingInfo(ADAM(0.0001, (0.9, 0.8)), Flux.params(model))
-    LearningAgent(model, [], [], model_train)
+    model_train = ModelTrainingInfo(ADAM(0.001, (0.9, 0.8)), Flux.params(model))
+    LearningAgent(model, [], [], model_train, 0.5)
 end
 
 function get_values(state::LearningAgent, states)
@@ -49,20 +61,26 @@ end
 
 function get_action(state::LearningAgent, game_state, actions)
     board = game_state.board
-    next_states = map(action -> Connect4.act(game_state, action)[1], actions)
-    values = get_values(state, next_states)
-    (_, index) = findmax(values)
-    if false
-        println(game_state)
-        println(values)
-        println(index[2])
-        println("--------")
+    if rand() < state.exploration_rate
+        index = rand(1:length(actions))
+        actions[index]
+    else
+        next_states = map(action -> Connect4.act(game_state, action)[1], actions)
+        values = get_values(state, next_states)
+        (_, index) = findmax(values)
+        if false
+            println(game_state)
+            println(values)
+            println(index[2])
+            println("--------")
+        end
+        actions[index[2]] 
     end
-    actions[index[2]] 
 end
 
 function push_state!(state::LearningAgent, game_state)
-    LearningAgent(state.model, push!(state.game_states, game_state), state.episode_results, state.model_train)
+    LearningAgent(state.model, push!(state.game_states, game_state), 
+        state.episode_results, state.model_train, state.exploration_rate)
 end
 
 function end_episode(state::LearningAgent, reward)
@@ -73,9 +91,9 @@ function end_episode(state::LearningAgent, reward)
     episode_results = push!(state.episode_results, episode_result)
     if length(episode_results) >= 300
         train!(state)
-        LearningAgent(state.model, [], [], state.model_train)
+        LearningAgent(state.model, [], [], state.model_train, state.exploration_rate)
     else
-        LearningAgent(state.model, [], episode_results, state.model_train)
+        LearningAgent(state.model, [], episode_results, state.model_train, state.exploration_rate * 0.99995)
     end
 end
 
